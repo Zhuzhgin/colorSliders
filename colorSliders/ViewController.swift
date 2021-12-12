8881c9112284399223c87804f4c1edfa0f73bdb3
Zhuzhgin <artemfox@list.ru>
2021-12-12 12:45:37 +0500

    Initial Commit

---
+++ colorSliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  colorSliders
+//
+//  Created by Artem Zhuzhgin on 12.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
