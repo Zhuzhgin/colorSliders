8c51d00dffa5a3633190fd8f3e80e92c3de9db5a
Zhuzhgin <artemfox@list.ru>
2021-12-12 22:34:32 +0500

    SliderColors

--- colorSliders/ViewController.swift
+++ colorSliders/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var viewColor: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    
+    @IBOutlet var greenValuelabel: UILabel!
+    
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    
+    @IBOutlet var greenSlider: UISlider!
+    
+    @IBOutlet var blueSlider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
+        viewColor.layer.cornerRadius = 10
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
-
+    @IBAction func redSliderMove(_ sender: UISlider) {
+        viewColor.backgroundColor = UIColor.init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        
+        var roundTwoNambers = round(redSlider.value * 100) / 100
+        redValueLabel.text = String(roundTwoNambers)
+        
+        
+    }
+    
+  
+    @IBAction func grenSliderMove(_ sender: UISlider) {
+        viewColor.backgroundColor = UIColor.init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        var roundTwoNambers = round(greenSlider.value * 100) / 100
+        greenValuelabel.text = String(roundTwoNambers)
+    }
+    
+    @IBAction func blueSliderMove(_ sender: UISlider) {
+        viewColor.backgroundColor = UIColor.init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        var roundTwoNambers = round(blueSlider.value * 100) / 100
+        blueValueLabel.text = String(roundTwoNambers)
+    }
+    
 }
 
